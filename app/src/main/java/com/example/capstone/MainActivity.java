cea672944c28e2a273b64b1ecf54262520f12a02
Jasek <02jasek.zielaskowski@gulllakecs.org>
2021-05-27 11:05:02 -0400

    Added checkboxes for menu and java code for when clicked

--- app/src/main/java/com/example/capstone/MainActivity.java
+++ app/src/main/java/com/example/capstone/MainActivity.java
@@ -11,4 +11,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
+
+    int quantity = 0;
+    private int basePrice;
+
+
+    private int calculatePrice(boolean chicken_wings_checkbox, boolean burger_checkbox, boolean tacos_checkbox, boolean fish_checkbox) {
+        int basePrice = 0;
+        if (chicken_wings_checkbox) {
+            basePrice = basePrice + 10;
+        }
+        if (burger_checkbox) {
+            basePrice = basePrice + 7;
+        }
+        if (tacos_checkbox) {
+            basePrice = basePrice + 9;
+        }
+        if (fish_checkbox) {
+            basePrice = basePrice + 12;
+        }
+        return quantity * basePrice;
+    }
+
+
+
+
+
 }
